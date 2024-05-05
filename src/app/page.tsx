4f7d7a20795d837ae2ae13ce63c5ee8517494d46
annaSemenova <anna.7nova@yandex.by>
2024-05-05 22:32:29 +0300

    Create visual part of component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@nextui-org/react";
 import * as action from "@/actions";
 import { auth } from "@/auth";
+import { db } from "../db";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
+  const posts = await db.post.findMany();
   return (
     <main>
       <div className="flex justify-between items-center bg-gray-100 rounded-lg">
@@ -26,6 +29,51 @@ export default async function Home() {
           <div>Signed out</div>
         )}
       </div>
+      <div className="border p-2 border-rounded">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-xl font-bold">Top Posts</h2>
+          <Button className="border p-2 border-rounded">New Topic</Button>
+        </div>
+        <div className="flex flex-row justify-between">
+          <div className="flex flex-col grow">
+            <div className="items-center p-2 border rounded">
+              <div>{posts.map(({ id, title, user, comments }) => (
+                <Link className="items-center p-2 border rounded" key={id} href={`/posts/${id}`}>
+                  <p className="font-bold">{title}</p>
+                  <div className="flex flex-row justify-between">
+                    <div>{user}</div>
+                    <div>{comments}</div>
+                  </div>
+                </Link>
+              ))}
+              </div>
+            </div>
+
+            <div className="items-center p-2 border rounded">
+              <p className="font-bold">Implementing Charts</p>
+              <div className="flex flex-row justify-between">
+                <div>By wpa</div>
+                <div>20 comments</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="items-center p-2 border rounded">
+              <h3 className="text-l font-bold">Topics</h3>
+              <ul>
+                <li>javascript</li>
+                <li>golang</li>
+                <li>servers</li>
+                <li>webdev</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </main>
   );
 }
+
+
--- src/app/topics/javascript/page.tsx
+++ src/app/topics/javascript/page.tsx
@@ -7,20 +7,43 @@ export default async function TopicView() {
     console.log(posts);
 
     return (
-        <div className="flex flex-row justify-between">
-            <div className="flex flex-col grow">
-                <h2>javascript</h2>
-                <div>
-                    <p>Implementing Charts</p>
-                </div>
+        <div >
+            <div className="flex flex-row justify-between items-center">
+                <h2 className="text-xl font-bold">javascript</h2>
+                <Link className="border p-2 border-rounded" href="/posts/new">Create Post</Link>
             </div>
-            <div className="flex flex-col grow-0">
-                <Link className="border p-2 border-rounded" href="#">Create Post</Link>
-                <div>
-                    <h3>javascript</h3>
-                    <p>Here you can discuss...</p>
+
+            <div className="flex flex-row justify-between">
+                <div className="flex flex-col grow">
+                    <div className="items-center p-2 border rounded">
+                        <div>{posts.map(({ id, title, user, comments }) => (
+                            <Link className="items-center p-2 border rounded" key={id} href={`/posts/${id}`}>
+                                <p className="font-bold">{title}</p>
+                                <div className="flex flex-row justify-between">
+                                    <div>{user}</div>
+                                    <div>{comments}</div>
+                                </div>
+                            </Link>
+                        ))}
+                        </div>
+                    </div>
+                    <div className="items-center p-2 border rounded">
+                        <p className="font-bold">Implementing Charts</p>
+                        <div className="flex flex-row justify-between">
+                            <div>By wpa</div>
+                            <div>20 comments</div>
+                        </div>
+                    </div>
                 </div>
+
+                <div>
+                    <div className="items-center p-2 border rounded">
+                        <h3 className="text-l font-bold">javascript</h3>
+                        <p>Here you can discuss...</p>
+                    </div>
+                </div>
+
             </div>
         </div>
-    )
+    );
 }
