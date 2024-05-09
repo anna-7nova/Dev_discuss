27b3913dd50100b0ffe5c2489e470512c562c4f3
badunka-learnetic <122442934+badunka-learnetic@users.noreply.github.com>
2024-05-09 19:42:54 +0200

    add generalHeader

---
+++ src/app/components/generalHeader.tsx
@@ -0,0 +1,23 @@
+
+import { auth } from '@/auth';
+import HeaderComponent from "./header";
+
+interface Session {
+
+}
+export default async function GeneralHeader() {
+
+  let session: Session | null = await auth();
+  let dataSession = null
+  if (session !== null) {
+    dataSession = session
+  } else {
+    dataSession = ''
+  }
+  return (
+
+    <main>
+      <HeaderComponent  data={dataSession}/>
+    </main>
+ )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,16 @@ import * as action from '@/actions';
 import { auth } from '@/auth';
 import { Button, useDisclosure } from '@nextui-org/react';
 import ModalCreateTopic from './components/modal-create-topic';
-import HeaderComponent from "./components/header";
+import GeneralHeader from './components/generalHeader';
 
-interface Session {
 
-}
 export default async function Home() {
   const { isOpen, onOpenChange, onOpen } = useDisclosure();
-  let session: Session | null = await auth();
-  let dataSession = null
-  if (session !== null) {
-    dataSession = session
-  } else {
-    dataSession = ''
-  }
+
   return (
 
     <main>
-      <HeaderComponent  data={dataSession}/>
+      <GeneralHeader/>
       <Button onPress={onOpen} color='primary'>
         New Topic
       </Button>
