7ec16790a765ee020d14dff591eaa462ec3bb603
Katsiaryna Voronich <katarinka403@gmail.com>
2024-05-30 15:08:15 +0300

    feat: add post list

--- src/app/components/post-card.tsx
+++ src/app/components/post-card.tsx
@@ -13,12 +13,14 @@ export default async function PostCard({ id, title, userId, topicId }: PostCardP
   const user = await db.user.findUnique({ where: { id: userId } });
   const comments = await db.comment.findMany({ where: { postId: id } });
   return (
-    <Link className='items-center p-2 border rounded' key={id} href={`topics/${topic?.slug}/posts/${id}`}>
-      <p className='font-bold'>{title}</p>
-      <div className='flex flex-row justify-between'>
-        <div>{user?.name || 'Anonymous'}</div>
-        <div>{comments.length}</div>
-      </div>
-    </Link>
+    <div className='items-center p-2 border rounded'>
+      <Link className='items-center p-2 border rounded' key={id} href={`topics/${topic?.slug}/posts/${id}`}>
+        <p className='font-bold'>{title}</p>
+        <div className='flex flex-row justify-between'>
+          <div>{user?.name || 'Anonymous'}</div>
+          <div>{comments.length}</div>
+        </div>
+      </Link>
+    </div>
   );
 }
