15b9afaff801b8263fcd89b3afde85b776bd3643
Inna Barysenka <inna.barysenka@gmail.com>
2024-06-04 11:32:03 +0300

    feat: add an opportunity to leave and see comments, show user name

--- src/app/components/current_post.tsx
+++ src/app/components/current_post.tsx
@@ -1,30 +1,23 @@
-import {createPost, createComment} from "@/app/actions";
-import {db} from "@/db"
+import { db } from "@/db";
 import { notFound } from "next/navigation";
-import CommentCreation from "@/app/components/comments_form";
 
 interface PostProps {
- postId: string;
+
+  postId: string;
 }
 
-export default async function ViewPost({postId} : PostProps) {
-const post = await db.post.findFirst({
-  where: {id: postId}
- })
+export default async function ViewPost({  postId }: PostProps) {
+  const post = await db.post.findFirst({
+    where: { id: postId },
+  });
 
-// if(!post) {
-//notFound();
-// }
+  if (!post) {
+    notFound();
+  }
   return (
-      <div className="flex flex-col gap-5">
-       <h3 style={{ fontSize: "20", fontWeight: "600" }}>
-          {/*post.title*/} Implementing charts
-        </h3>
-        <p style={{ fontSize: "16" }}>
-          {/*post.content*/}I am trying to add a charts into my application, can
-          somebody help me out
-        </p>
-        </div>
-
+    <div className="flex flex-col gap-5">
+      <h3 style={{ fontSize: "20", fontWeight: "600" }}>{post.title}</h3>
+      <p style={{ fontSize: "16" }}>{post.content}</p>
+    </div>
   );
 }
